aea380cbebedf11fee46c53fa299418cf16c7104
Lhasang Tulku Lama <lamalhasang256@gmail.com>
2024-08-07 21:22:58 +0545

    added

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./favicon.ico"></link>
+      <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
       <Analytics />
